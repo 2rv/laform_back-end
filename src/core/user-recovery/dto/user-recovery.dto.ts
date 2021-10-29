ed857b15c1c087e7707289fc8637e09563e64b1f
TAbrahamyan <t.abrahamyann@gmail.com>
2021-10-29 19:47:57 +0400

    Updated user recovery email validation

--- src/core/user-recovery/dto/user-recovery.dto.ts
+++ src/core/user-recovery/dto/user-recovery.dto.ts
@@ -1,8 +1,11 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class UserRecoveryDto {
   @IsNotEmpty()
   @IsString()
   @IsEmail()
+  @Transform((value: string) => value?.toLowerCase())
+  @Transform((value: string) => value?.trim())
   email: string;
 }
