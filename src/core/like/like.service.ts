ce49bed2255ac570b936fa013065ac6ea224c2f8
pasha1neo <pasha1neo@mail.ru>
2021-10-30 20:15:48 +0300

    fix

--- src/core/like/like.service.ts
+++ src/core/like/like.service.ts
@@ -1,22 +1,12 @@
 import { LikeRepository } from './like.repository';
 import { Injectable, BadRequestException } from '@nestjs/common';
-import { MasterClassRepository } from './../master-class/master-class.repository';
 import { LIKE_ERROR } from './enum/like.enum';
-import { PostRepository } from './../post/post.repository';
-import { PatternProductRepository } from '../pattern-product/pattern-product.repository';
-import { SewingProductRepository } from '../sewing-product/sewing-product.repository';
 import { LikeDto } from './dto/like.dto';
 import { LikeEntity } from './like.entity';
 
 @Injectable()
 export class LikeService {
-  constructor(
-    private likeRepository: LikeRepository,
-    private postRepository: PostRepository,
-    private masterClassRepository: MasterClassRepository,
-    private sewingProductRepository: SewingProductRepository,
-    private patternProductRepository: PatternProductRepository,
-  ) {}
+  constructor(private likeRepository: LikeRepository) {}
 
   async create(body: LikeDto, userId) {
     const result = await this.likeRepository.findOne({
@@ -26,27 +16,17 @@ export class LikeService {
     if (result) {
       throw new BadRequestException(LIKE_ERROR.LIKE_ALREADY_EXISTS);
     } else await this.likeRepository.save({ ...body, userId: userId });
-    // const count = await this.count(body);
+
     if (body.postId) {
-      //   await this.postRepository.update(body.postId, { likeCount: count });
       return { id: body.postId };
     }
     if (body.masterClassId) {
-      //   await this.masterClassRepository.update(body.masterClassId, {
-      //     likeCount: count,
-      //   });
       return { id: body.masterClassId };
     }
     if (body.sewingProductId) {
-      //   await this.sewingProductRepository.update(body.sewingProductId, {
-      //     likeCount: count,
-      //   });
       return { id: body.sewingProductId };
     }
     if (body.patternProductId) {
-      //   await this.patternProductRepository.update(body.patternProductId, {
-      //     likeCount: count,
-      //   });
       return { id: body.patternProductId };
     }
   }
@@ -64,27 +44,17 @@ export class LikeService {
     if (!result) {
       throw new BadRequestException(LIKE_ERROR.LIKE_NOT_EXISTS);
     } else await this.likeRepository.delete(result.id);
-    // const count = await this.count(body);
+
     if (body.postId) {
-      //   await this.postRepository.update(body.postId, { likeCount: count });
       return { id: body.postId };
     }
     if (body.masterClassId) {
-      //   await this.masterClassRepository.update(body.masterClassId, {
-      //     likeCount: count,
-      //   });
       return { id: body.masterClassId };
     }
     if (body.sewingProductId) {
-      //   await this.sewingProductRepository.update(body.sewingProductId, {
-      //     likeCount: count,
-      //   });
       return { id: body.sewingProductId };
     }
     if (body.patternProductId) {
-      //   await this.patternProductRepository.update(body.patternProductId, {
-      //     likeCount: count,
-      //   });
       return { id: body.patternProductId };
     }
   }
