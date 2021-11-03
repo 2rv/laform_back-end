a22848be3131d210e31e338ae40ae6e5b96f5a07
Evgeny Arol <arolevgeny@gmail.com>
2021-11-03 21:04:53 +0300

    edit apple controller

--- src/core/auth/apple.strategy.ts
+++ src/core/auth/apple.strategy.ts
@@ -15,6 +15,7 @@ export class AppleStrategy extends PassportStrategy(Strategy, 'apple') {
       keyID: AppleConfig.keyID,
       privateKeyLocation: path.join(__dirname, '../../../config/AuthKey.p8'),
       passReqToCallback: true,
+      scope: ['email', 'profile'],
       // clientID: '1042068275751-c6pbac6s5l3bjvo73amvl77f3ol2e8dj.apps.googleusercontent.com',
       // clientSecret: 'pQRjjsvJLxydvHMESpajKikM',
       // callbackURL: 'http://localhost:4000/auth/google/redirect',
@@ -31,11 +32,10 @@ export class AppleStrategy extends PassportStrategy(Strategy, 'apple') {
     const { name, emails } = profile;
     const user = {
       email: emails[0].value,
-      firstName: name.givenName,
-      lastName: name.familyName,
       accessToken,
+      idToken,
       id: profile.id,
     };
-    cb(null, profile);
+    cb(null, user);
   }
 }
