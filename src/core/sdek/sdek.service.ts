02ff861f45cba337bb16027be7230923718df68f
Ivan <75670551+KokniJS@users.noreply.github.com>
2021-11-30 22:10:23 +0300

    fix

--- src/core/sdek/sdek.service.ts
+++ src/core/sdek/sdek.service.ts
@@ -120,6 +120,9 @@ export class SdekService {
           },
         },
       );
+      if (getOffices.data == null || getOffices.data == undefined) {
+        return [];
+      }
       return getOffices.data;
     } catch (err) {
       throw new InternalServerErrorException(err);
