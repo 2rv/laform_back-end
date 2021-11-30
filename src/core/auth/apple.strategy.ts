0803d249fb3f02f1191697d9cd16edd2770fed5e
Ivan <75670551+KokniJS@users.noreply.github.com>
2021-12-01 01:31:27 +0300

    fix problem with apple auth

--- src/core/auth/apple.strategy.ts
+++ src/core/auth/apple.strategy.ts
@@ -15,7 +15,7 @@ export class AppleStrategy extends PassportStrategy(Strategy, 'apple') {
       keyID: AppleConfig.keyID,
       privateKeyLocation: path.join(__dirname, '../../../config/AuthKey.p8'),
       //passReqToCallback: true,
-      scope: ['email', 'name', 'profile'],
+      scopes: ['email', 'name', 'profile'],
       // clientID: '1042068275751-c6pbac6s5l3bjvo73amvl77f3ol2e8dj.apps.googleusercontent.com',
       // clientSecret: 'pQRjjsvJLxydvHMESpajKikM',
       // callbackURL: 'http://localhost:4000/auth/google/redirect',
