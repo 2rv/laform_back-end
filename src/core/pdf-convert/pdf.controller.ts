ed2e0e3f5cbb5edf68fabe31fe7245fefb3e3402
Evgeny Arol <arolevgeny@gmail.com>
2021-12-06 03:25:09 +0300

    comment pdf

--- src/core/pdf-convert/pdf.controller.ts
+++ src/core/pdf-convert/pdf.controller.ts
@@ -14,95 +14,95 @@ import { AuthGuard } from '@nestjs/passport';
 import { AccountGuard } from '../user/guard/account.guard';
 import { USER_ROLE } from '../user/enum/user-role.enum';
 
-const request = require('request-promise');
-const fs = require('fs');
-const path = require('path');
+// const request = require('request-promise');
+// const fs = require('fs');
+// const path = require('path');
 
 @Controller('pdf')
 export class PdfController {
   constructor(private sdekService: SdekService) {}
 
-  @Post('aws')
-  @Roles(USER_ROLE.ADMIN, USER_ROLE.USER)
-  @UseGuards(AuthGuard('jwt'), AccountGuard)
-  async amzaonPDF(@Body() body, @Res() res) {
-    request
-      .get({
-        url: body.url,
-        encoding: null,
-      })
-      .then((response) => {
-        try {
-          const dir = './output';
+  // @Post('aws')
+  // @Roles(USER_ROLE.ADMIN, USER_ROLE.USER)
+  // @UseGuards(AuthGuard('jwt'), AccountGuard)
+  // async amzaonPDF(@Body() body, @Res() res) {
+  //   request
+  //     .get({
+  //       url: body.url,
+  //       encoding: null,
+  //     })
+  //     .then((response) => {
+  //       try {
+  //         const dir = './output';
 
-          if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
-          }
-          const date = new Date();
-          fs.writeFileSync('./output/' + date + '.pdf', response);
-          console.log('Success in writing file');
+  //         if (!fs.existsSync(dir)) {
+  //           fs.mkdirSync(dir, { recursive: true });
+  //         }
+  //         const date = new Date();
+  //         fs.writeFileSync('./output/' + date + '.pdf', response);
+  //         console.log('Success in writing file');
 
-          const filePath = path.join(
-            __dirname,
-            '../../../output/' + date + '.pdf',
-          );
+  //         const filePath = path.join(
+  //           __dirname,
+  //           '../../../output/' + date + '.pdf',
+  //         );
 
-          res.sendFile(filePath, (err) => {
-            if (err) {
-              console.log(err);
-            }
-            fs.unlinkSync(filePath, () => {
-              console.log('File was deleted');
-            });
-          });
-        } catch (err) {
-          console.log('Error in writing file');
-          console.log(err);
-        }
-      });
-  }
+  //         res.sendFile(filePath, (err) => {
+  //           if (err) {
+  //             console.log(err);
+  //           }
+  //           fs.unlinkSync(filePath, () => {
+  //             console.log('File was deleted');
+  //           });
+  //         });
+  //       } catch (err) {
+  //         console.log('Error in writing file');
+  //         console.log(err);
+  //       }
+  //     });
+  // }
 
-  @Post('sdek')
-  @Roles(USER_ROLE.ADMIN, USER_ROLE.USER)
-  @UseGuards(AuthGuard('jwt'), AccountGuard)
-  async sdekPDF(@Body() body, @Res() res) {
-    request
-      .get({
-        url: body.url,
-        encoding: null,
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: await this.sdekService.authInSdek(),
-        },
-      })
-      .then((response) => {
-        try {
-          const dir = './output';
+  // @Post('sdek')
+  // @Roles(USER_ROLE.ADMIN, USER_ROLE.USER)
+  // @UseGuards(AuthGuard('jwt'), AccountGuard)
+  // async sdekPDF(@Body() body, @Res() res) {
+  //   request
+  //     .get({
+  //       url: body.url,
+  //       encoding: null,
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Authorization: await this.sdekService.authInSdek(),
+  //       },
+  //     })
+  //     .then((response) => {
+  //       try {
+  //         const dir = './output';
 
-          if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
-          }
-          const date = new Date();
-          fs.writeFileSync('./output/' + date + '.pdf', response);
-          console.log('Success in writing file');
+  //         if (!fs.existsSync(dir)) {
+  //           fs.mkdirSync(dir, { recursive: true });
+  //         }
+  //         const date = new Date();
+  //         fs.writeFileSync('./output/' + date + '.pdf', response);
+  //         console.log('Success in writing file');
 
-          const filePath = path.join(
-            __dirname,
-            '../../../output/' + date + '.pdf',
-          );
+  //         const filePath = path.join(
+  //           __dirname,
+  //           '../../../output/' + date + '.pdf',
+  //         );
 
-          res.sendFile(filePath, (err) => {
-            if (err) {
-              console.log(err);
-            }
-            fs.unlinkSync(filePath, () => {
-              console.log('File was deleted');
-            });
-          });
-        } catch (err) {
-          console.log('Error in writing file');
-          console.log(err);
-        }
-      });
-  }
+  //         res.sendFile(filePath, (err) => {
+  //           if (err) {
+  //             console.log(err);
+  //           }
+  //           fs.unlinkSync(filePath, () => {
+  //             console.log('File was deleted');
+  //           });
+  //         });
+  //       } catch (err) {
+  //         console.log('Error in writing file');
+  //         console.log(err);
+  //       }
+  //     });
+  // }
 }
