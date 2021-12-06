7a719b2567655b3ce5daca6be9555551d050dfdd
pasha1neo <pasha1neo@mail.ru>
2021-12-06 07:34:58 +0300

    adress-saved

--- src/core/user-info/user-info.service.ts
+++ src/core/user-info/user-info.service.ts
@@ -38,38 +38,8 @@ export class UserInfoService {
         userId: user.id,
       },
     });
+    console.log(body);
     await this.userInfoRepository.update(result.id, body);
     return await this.findOne(user);
   }
 }
-// const testData = {
-// 	country: {
-// 	  country: user.country,
-// 	  country_iso_code: user.country_iso_code,
-// 	  label: user.label_country,
-// 	},
-// 	city: {
-// 	  city: user.city,
-// 	  fias_id: user.fias_id,
-// 	  fias_level: user.fias_level_city,
-// 	  kladr_id: user.kladr_id,
-// 	  label: user.label_city,
-// 	  settlement: user.settlement,
-// 	},
-// 	street: {
-// 	  fias_id: user.fias_id_street,
-// 	  fias_level: user.fias_level_street,
-// 	  label: user.label_street,
-// 	  street: user.street,
-// 	},
-// 	house: {
-// 	  fias_id: user.fias_id_house,
-// 	  fias_level: user.fias_level_house,
-// 	  house: user.house,
-// 	  label: user.label_house,
-// 	},
-// 	postal_code: {
-// 	  label: user.label_postal_code,
-// 	  postal_code: user.postal_code,
-// 	},
-//   };
