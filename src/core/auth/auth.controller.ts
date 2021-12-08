9adc084c5920f8b8cc5cbc173b31743c1a4d16fe
Ivan <75670551+KokniJS@users.noreply.github.com>
2021-12-09 00:52:55 +0300

    fix problem with apple

--- src/core/auth/auth.controller.ts
+++ src/core/auth/auth.controller.ts
@@ -122,10 +122,13 @@ export class AuthController {
     //   accessToken: req.user.accessToken,
     // };
     //return res.send(req);
+    console.log(req.user);
     const token = await this.authService.signUpWithApple(req.user);
     const clientUrl = req.hostname.includes('localhost')
       ? `${req.protocol}://localhost:3000`
       : ClientConfig.url;
-    return res.redirect(`${clientUrl}/social-auth-access?data=${token.accessToken}`);
+    return res.redirect(
+      `${clientUrl}/social-auth-access?data=${token.accessToken}`,
+    );
   }
 }
