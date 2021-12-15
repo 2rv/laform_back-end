62c6abc5cc1e85d6d785530e425cb1fe6fc64abf
pasha1neo <pasha1neo@mail.ru>
2021-12-15 18:28:21 +0300

    just-test-expired-mk

--- src/core/payment/payment.service.ts
+++ src/core/payment/payment.service.ts
@@ -79,7 +79,8 @@ export class PaymentService {
       for (let purchaseProduct of product.purchaseProducts) {
         if (purchaseProduct.type === 0) {
           const expired = new Date();
-          expired.setMonth(expired.getMonth() + 6);
+          //   expired.setMonth(expired.getMonth() + 6);
+          expired.setMinutes(expired.getMinutes() + 5);
           await this.purchaseProductRepository.update(purchaseProduct.id, {
             expiredDate: expired,
           });
